f149b3bb35c1fa5049d3c5023030485624089baa
Katsuyo . <matheisdethier03@gmail.com>
2024-04-20 09:46:19 +0200

    Remove login edit

--- 1_BaseProjet_AFournir/Views/Users/inscriptionOrEditProfil.php
+++ 1_BaseProjet_AFournir/Views/Users/inscriptionOrEditProfil.php
@@ -19,10 +19,12 @@
             <label for="email" class="form-label">Email</label>
             <input type="email" placeholder="Email" class="form-control" id="email" name="email" required <?php if (isset($_SESSION['user'])) : ?>value="<?= $_SESSION['user']->emailUser ?>" <?php endif ?>>
         </div>
-        <div class="mb-3">
-            <label for="Login" class="form-label">Login</label>
-            <input type="text" placeholder="Login" class="form-control" id="login" name="login" required <?php if (isset($_SESSION['user'])) : ?>value="<?= $_SESSION['user']->loginUser ?>" <?php endif ?>>
-        </div>
+        <?php if (!isset($_SESSION["user"])) : ?>
+            <div class="mb-3">
+                <label for="Login" class="form-label">Login</label>
+                <input type="text" placeholder="Login" class="form-control" id="login" name="login" required <?php if (isset($_SESSION['user'])) : ?>value="<?= $_SESSION['user']->loginUser ?>" <?php endif ?>>
+            </div>
+        <?php endif ?>
         <div class="mb-3">
             <label for="Password" class="form-label">Mot de passe</label>
             <input type="password" placeholder="Mot de passe" class="form-control" id="mot_de_passe" name="mot_de_passe" required <?php if (isset($_SESSION['user'])) : ?>value="<?= $_SESSION['user']->passWordUser ?>" <?php endif ?>>
--- 1_BaseProjet_AFournir/Views/Users/profil.php
+++ 1_BaseProjet_AFournir/Views/Users/profil.php
@@ -63,10 +63,10 @@
 <div class="titre">Page Profil</div>
 <div class="cadre">
       <ul>
+            <li class="profil-li"><strong>Login : </strong><?php if (isset($_SESSION['user'])) : ?><?= $_SESSION['user']->loginUser ?> <?php endif ?></li>
             <li class="profil-li"><strong>Nom : </strong><?php if (isset($_SESSION['user'])) : ?><?= $_SESSION['user']->nomUser ?> <?php endif ?></li>
             <li class="profil-li"><strong>Prénom : </strong><?php if (isset($_SESSION['user'])) : ?><?= $_SESSION['user']->prenomUser ?> <?php endif ?></li>
             <li class="profil-li"><strong>Email : </strong><?php if (isset($_SESSION['user'])) : ?><?= $_SESSION['user']->emailUser ?> <?php endif ?></li>
-            <li class="profil-li"><strong>Login : </strong><?php if (isset($_SESSION['user'])) : ?><?= $_SESSION['user']->loginUser ?> <?php endif ?></li>
             <li class="profil-li last"><strong>Mot de passe : </strong><?php if (isset($_SESSION['user'])) : ?><?= $_SESSION['user']->passWordUser ?> <?php endif ?></li>
       </ul>
       <div class="btn-container">
